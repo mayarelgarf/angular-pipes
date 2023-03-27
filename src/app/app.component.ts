90fa94871c3a29dba65e296b2dae2a2db2b5559a
mayarelgarf <mayarmahmoud123@gmail.com>
2023-03-27 13:23:48 +0200

    1st commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  date="" ;
+  amount=0;
+
+
+  recieverName= "";
+
+  onEventChange(event:Event){
+    let target = event.target as HTMLInputElement;
+    this.recieverName = target.value;
+  }
+
+  onDateChange(event:Event){
+    let target = event.target as HTMLInputElement;
+    this.date = target.value;
+
+  }
+  onAmountChange(event:Event){
+    let target = event.target as HTMLInputElement;
+    this.amount = parseFloat(target.value) ;
+  }
 }
